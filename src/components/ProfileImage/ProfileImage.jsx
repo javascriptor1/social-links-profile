8777a99147ee0d540bd917ea69a1fec3a1945383
Mohammed Fakih <mhd.fakih@gmail.com>
2024-02-06 23:35:27 +0300

    update alt text

--- src/components/ProfileImage/ProfileImage.jsx
+++ src/components/ProfileImage/ProfileImage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './ProfileImage.css'
 
 const ProfileImage = () => {
-  return <img src="./assets/images/avatar-jessica.jpeg" alt=""/>
+  return <img src="./assets/images/avatar-jessica.jpeg" alt="Avatar of Jessica"/>
 };
 
 export default ProfileImage;
