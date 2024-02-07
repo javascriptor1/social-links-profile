5f4c50a48d11585f3d43595f0f658f54785264dc
Mohammed Fakih <mhd.fakih@gmail.com>
2024-02-07 12:44:29 +0300

    change p tag to h1

--- src/components/ProfileDetails/ProfileDetails.jsx
+++ src/components/ProfileDetails/ProfileDetails.jsx
@@ -4,7 +4,7 @@ import './ProfileDetails.css'
 const ProfileDetails = () => {
   return (
     <>
-      <p>Jessica Randall</p>
+      <h1>Jessica Randall</h1>
       <p>London, United Kingdom</p>
       <p>"Front-end developer and avid reader."</p>
     </>
